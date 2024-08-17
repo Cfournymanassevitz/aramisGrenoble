e02aecfc2f74091b9c79753d5e2f4d216e5d161e
Cfournymanassevitz <carole.fourny-manassevitz@le-campus-numerique.fr>
2024-08-17 20:05:46 +0200

    avancement gre

--- ventes/views.py
+++ ventes/views.py
@@ -1,7 +1,23 @@
+import os
+import json
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.template import loader
+
+from ventes.utils import get_agency_names
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the ventes index.")
+    template = loader.get_template('ventes/template/index.html')
+    context = {
+        'agency_names': get_agency_names(),
+    }
+    return HttpResponse(template.render(context, request))
 
+
+def ventes_json(request):
+    file_path = os.path.join(os.path.dirname(__file__), '../data.json')
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    return JsonResponse(data, safe=False)
 
